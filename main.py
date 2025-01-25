e08e902a8fc5c6c71d40ce8f99b0094b4a128851
siriwimon02 <siriwimon_y@cmu.ac.th>
2025-01-25 14:25:00 +0700

    add diff by B

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def main():
     num1 = int(input("Enter your number 1:"))
     num2 = int(input("Enter your number 2:"))
     
-    # change this line to call method
+    diff(num1,num2)
 
     # Member A call method add() at line: 8
     # Member B call method diff() at line: 8
     # Member C call method mul() at line: 8
+
     
     pass
 
